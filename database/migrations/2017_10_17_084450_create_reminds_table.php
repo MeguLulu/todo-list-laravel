094f0951c4f6b405f371d75bff7edffc8aff65f5
Venrog Megu <steven.proag@gmail.com>
2017-10-17 10:55:20 +0200

    creation de la migration reminds et modif eventcontroller

--- database/migrations/2017_10_17_084450_create_reminds_table.php
+++ database/migrations/2017_10_17_084450_create_reminds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class CreateRemindsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('reminds', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->dateTime('begin');
-            $table->dateTime('end');
+            $table->date('day');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -30,6 +29,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('reminds');
     }
 }
