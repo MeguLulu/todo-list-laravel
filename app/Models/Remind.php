15dcebe7e324be8f1a81e7f727f9e798a67ca4c8
Venrog Megu <steven.proag@gmail.com>
2017-10-18 11:20:35 +0200

    ajout du soft delete p2

--- app/Models/Remind.php
+++ app/Models/Remind.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Remind extends Model
 {
   use SoftDeletes;
-  
+
   protected $fillable = [
     'title',
     'day'
   ];
+  /**
+  *
+  * @var array
+  */
+  protected $dates = ['deleted_at'];
+
   // Renvoie la date pour le tri
   public function date() {
     return $this->day;
